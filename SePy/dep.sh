71fe747c44ff0b515d62fc8b1ef5a123ea34b6b3
Bodia2cat <62301981+Bodia2cat@users.noreply.github.com>
2020-04-12 20:56:22 +0300

    v2

---
+++ SePy/dep.sh
@@ -0,0 +1,17 @@
+apt install python3
+
+echo [+]start
+echo ------
+echo ----
+echo ---
+echo --
+echo -
+
+pip3 install colorama
+apt install w3m
+
+echo [+]Sucsessful
+
+clear
+
+python3 search2.py
